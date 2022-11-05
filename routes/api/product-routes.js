fd0cb361efd1ff40d953c7ff9af4c59e0c410335
Dylan Crowley <dcrowdev1025@gmail.com>
2022-11-05 09:50:35 -0400

    updated product-routes route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+    try {
+    const data = await Product.findAll({
   // be sure to include its associated Category and Tag data
+      include: [{ model: Category }, { model: ProductTag }, { model: Tag }]
+    });
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+    try {
+    const data = await Product.findByPk(req.params.id, {
   // be sure to include its associated Category and Tag data
+      include: [{ model: Category }, { model: ProductTag }, { model: Tag }]
+    });
+    if (!data) {
+      res.status(404).json({ message: 'No product found with that id!' })
+    };
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
